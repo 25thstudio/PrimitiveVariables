d52b71022c8a17195090cbd45934a08d25288da6
Paulo Sigrist <paulo.sigrist@gmail.com>
2021-02-06 08:01:17 -0300

    First commit

---
+++ Packages/com.alicate4.primitivevariables/Assets/Scripts/FloatVariable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alicate4.PrimitiveVariables
+{
+    public class FloatVariable : ScriptableObject
+    {
+        #region Properties
+        [Multiline]
+        [SerializeField]
+        private string description;
+        
+        [SerializeField]
+        private float floatValue;
+
+        #endregion
+
+        #region Setters and getters
+        public float Value { get => floatValue; set => floatValue = value; }
+
+        public float Increase(float value = 0.0f)
+        {
+            floatValue += value;
+            return floatValue;
+        }
+
+        public float Decrease(float value = 0.0f)
+        {
+            floatValue -= value;
+            return floatValue;
+        }
+        #endregion
+
+
+
+
+    }
+
+}
