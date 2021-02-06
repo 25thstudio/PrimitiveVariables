b54826bb1762027f120c964a75d67769ddcf911c
Paulo Sigrist <paulo.sigrist@gmail.com>
2021-02-06 17:52:31 -0300

    int variable

---
+++ Packages/com.alicate4.primitivevariables/Assets/Scripts/IntVariable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntVariable : ScriptableObject
+{
+    #region Properties
+    [Multiline]
+    [SerializeField]
+    private string description;
+
+    [SerializeField]
+    private int intValue;
+
+    #endregion
+
+    #region Setters and getters
+    public int Value { get => intValue; set => intValue = value; }
+
+    public int Increase(int value = 0)
+    {
+        intValue += value;
+        return intValue;
+    }
+
+    public int Decrease(int value = 0)
+    {
+        intValue -= value;
+        return intValue;
+    }
+    #endregion
+
+
+
+
+}
